0d1ce9c742351cb944e18617b7f84b7a154c4ae7
Chabam <fchabot1337@gmail.com>
2024-11-10 15:58:57 -0500

    Moved some procedures around

--- metacircular-evaluator.rkt
+++ metacircular-evaluator.rkt
@@ -81,14 +81,6 @@
     env)
   'ok)
 
-(define (application? exp) (pair? exp))
-(define (operator exp) (car exp))
-(define (operands exp) (cdr exp))
-
-(define (no-operands? ops) (null? ops))
-(define (first-operand ops) (car ops))
-(define (rest-operands ops) (cdr ops))
-
 (define (apply-primitive-procedure proc args)
   (apply-in-underlying-racket
    (primitive-implementation proc) args))
--- procedure.rkt
+++ procedure.rkt
@@ -7,6 +7,12 @@
 (provide procedure-parameters)
 (provide procedure-body)
 (provide procedure-environment)
+(provide application?)
+(provide operator exp)
+(provide operands exp)
+(provide no-operands?)
+(provide first-operand)
+(provide rest-operands)
 
 (define (make-procedure parameters body env)
   (list 'procedure parameters body env))
@@ -14,8 +20,14 @@
 (define (compound-procedure? p)
   (tagged-list? p 'procedure))
 
-
 (define (procedure-parameters p) (cadr p))
 (define (procedure-body p) (caddr p))
 (define (procedure-environment p) (cadddr p))
 
+(define (application? exp) (pair? exp))
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+
+(define (no-operands? ops) (null? ops))
+(define (first-operand ops) (car ops))
+(define (rest-operands ops) (cdr ops))
